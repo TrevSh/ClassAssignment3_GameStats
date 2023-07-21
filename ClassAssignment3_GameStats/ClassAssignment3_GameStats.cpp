444b4145fbd5396f5ffbb4f775cf795a30664021
TrevSh <trevonshelby@outlook.com>
2023-07-21 15:13:55 -0500

    Add project files.

---
+++ ClassAssignment3_GameStats/ClassAssignment3_GameStats.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int score;
+    double distance;
+    char playAgain;
+    bool shieldsUp;     //True or false
+
+    short lives, aliensKilled;
+
+    score = 0;
+    distance = 1200.76;
+    playAgain = 'y';
+    shieldsUp = true;
+    lives = 3;
+    aliensKilled = 10;
+
+    double engineTemp = 6572.89;
+}
